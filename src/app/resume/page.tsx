473868c609998e19118141c5f090116b33767784
Nelson Loop <nelsonploop@gmail.com>
2024-09-11 18:41:40 -0400

    Uploaded resume.

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material';
+import React from 'react';
+
+const ResumePage = () => {
+  return (
+    <Box height='100vh'>
+      <iframe title='Resume' src='/assets/resume.pdf' width='100%' height='100%' />
+    </Box>
+  );
+};
+
+export default ResumePage;
