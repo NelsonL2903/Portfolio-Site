c3a09ef8ed8ba3b4fa2eda56390eb5d1efe5456b
Nelson Loop <nelsonploop@gmail.com>
2025-05-09 15:40:55 -0400

    Fix build error.

--- src/components/about/skills.tsx
+++ src/components/about/skills.tsx
@@ -1,7 +1,7 @@
 import { Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import SkillGrid from './skill-grid';
-import Portrait from '@/assets/casual-portrait.jpg';
+import Portrait from '../../assets/casual-portrait.jpg';
 
 const Skills = (): JSX.Element => {
   return (
